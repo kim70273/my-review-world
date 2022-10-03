462fca27929f7318ccfd91cec7178e8633a84d54
김성현 <61579430+kim70273@users.noreply.github.com>
2022-10-03 15:24:05 +0900

    #3 기본페이지(뉴스, 관리자리뷰, 사용자리뷰, 자유게시판) 생성

---
+++ pages/community.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Community: NextPage = () => {
+  return <div>자유게시판</div>;
+};
+
+export default Community;
---
+++ pages/manager-review/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const ManagerReview: NextPage = () => {
+  return <div>관리자의 리뷰</div>;
+};
+
+export default ManagerReview;
